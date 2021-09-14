fd918b36c211e5332589c322efa8e5466b3fab55
Yulianaa7 <yuliananurhanifah77@gmail.com>
2021-09-14 11:41:46 +0700

    Transaksi Data

---
+++ produk.php
@@ -0,0 +1,28 @@
+<?php 
+    include "header_pelanggan.php";
+?>
+<h2>DAFTAR PRODUK</h2>
+<div class="row">
+    <?php 
+    include "koneksi.php";
+    //mengambil semua data dari tabel produk
+    $qry_produk=mysqli_query($conn,"select * from produk");
+    while($dt_produk=mysqli_fetch_array($qry_produk)){
+        ?>
+        <div class="col-md-3">
+            <div class="card" >
+              <img src="foto/<?=$dt_produk['foto_produk']?>" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title"><?=$dt_produk['nama_produk']?></h5>
+                <p class="card-text"><?=substr($dt_produk['deskripsi'], 0,100)?></p> 
+                <a href="beli_produk.php?id_produk=<?=$dt_produk['id_produk']?>" class="btn btn-primary">Beli</a>
+              </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+<?php 
+    include "footer_pelanggan.php";
+?>
